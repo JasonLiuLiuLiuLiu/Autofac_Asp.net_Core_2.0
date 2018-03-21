f2bd67acd2e34358ae119a5d77303562af73a117
liu zhenyu <liuzhenyulive@live.com>
2018-03-21 14:50:34 +0800

    在asp.net core 2.0下利用autofac进行依赖注入的例子

--- Autofac_Asp.net_Core_2.0/Startup.cs
+++ Autofac_Asp.net_Core_2.0/Startup.cs
@@ -1,29 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Service;
 
 namespace Autofac_Asp.net_Core_2._0
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            //create configuration
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
         }
 
         public IConfiguration Configuration { get; }
 
+        public IContainer ApplicationContainer { get; private set; }
+
+        //把返回的void更改为IServiceProvider
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            var assemblys = Assembly.Load("Service");    //service是程序集的名称
+            var baseType = typeof(IDAL);
+            builder.RegisterAssemblyTypes(assemblys)
+                .Where(m => baseType.IsAssignableFrom(m) && m != baseType)
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+            ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Service/MysqlDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service
+{
+    public class MysqlDAL:IDAL
+    {
+        public string Insert()
+        {
+          return  "MySqlDAL中的Insert方法";
+        }
+    }
+}
